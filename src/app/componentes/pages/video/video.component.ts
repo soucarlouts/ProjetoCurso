5cd90e5defd1f47ae68eab1d73c8722857f81944
JhonatanTana <jhonatan.tana@alunos.unis.edu.br>
2024-05-07 00:10:57 -0300

    update

---
+++ src/app/componentes/pages/video/video.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { AulasService } from "../../../services/aulas.service";
+
+@Component({
+  selector: 'app-video',
+  templateUrl: './video.component.html',
+  styleUrls: ['./video.component.css']
+})
+export class VideoComponent {
+  aula!: any;
+
+  constructor(
+    private aulasService: AulasService,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+
+    this.aulasService
+      .getAula(id).subscribe(item =>
+      (this.aula = item));
+  }
+}
